49cfcdaa068dbad7eebfe10f058a8bbafdcf8345
Paul Tilley <ptilley1@binghamton.edu>
2019-05-06 20:09:16 -0400

    erroneous

--- gh-pages/_site/src/includes.cc
+++
@@ -1,8 +0,0 @@
-#include <assert.h>
-#include <ctype.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
--- gh-pages/_site/tests/README.md
+++
@@ -1,6 +0,0 @@
-# Testing
-
-These tests can be run using the `lesen` testing suite, found here:
-
-https://github.com/pixlark/lesen
-
